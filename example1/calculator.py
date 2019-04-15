21e9db0a73ce66193ef892e76735c06f48f0b412
annkaranja <anniekrnj@gmail.com>
2019-04-15 17:24:13 +0300

    operations

---
+++ example1/calculator.py
@@ -0,0 +1,38 @@
+def add (a,b):
+	answer = a + b
+	return answer
+
+def wholenumberdivision (b,c):
+	answer = b//c
+	return answer
+	
+ def divide (c,d):
+	answer = c/d
+	return answer
+
+
+ def subtract (b,c):
+	answer = b-c
+	return answer
+
+
+ def modulus (b,c):
+	answer = b % c
+	return answer
+
+
+ def multiply (b,c):
+	answer = b * c
+	return answer
+
+
+ def exponent(b,c):
+	answer = b ** c
+	return answer
+
+
+ 
+ def percentage (k,l):
+	answer =  (k/l)*100
+	return answer
+
