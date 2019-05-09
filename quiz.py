626eb6a175b9962baf5977a84b5da2ee3096b1ab
annkaranja <47825070+annkaranja@users.noreply.github.com>
2019-05-09 18:52:49 +0300

    Add files via upload
    my data structures...lists,dictionaries,sets,tuples

---
+++ quiz.py
@@ -0,0 +1,54 @@
+def sorted():
+	a=['apple','banana','mango']
+	b=['avocado','peach','orange']
+	c=['pineapple','lemon']
+	d=set(a+b+c)
+	print(d)
+
+def print_dict():
+	students=[{'age':19,'name';'Eunice'},{'age':21,'name':'Agnes'},{'age':18,'name':'Teresa'},{'age';22,'name':'Asha'}]
+	for student in students:
+	print(“hello {}, you were born in year {}.”.format (student[name],student[age]))
+ 
+def smallest:
+	a=(1,3,6,8,2,4,5,7)
+	print(min.(a))
+
+def squares():
+	square=[for x**2 x in range(149,159)]
+    print(square)
+
+def	nest():
+	list_x = [[1,2],[3,4],[5,6]]
+	flat=[]
+	for list_x in nested:
+              for m in list_x
+              0=m
+              flat.append(0)
+
+def divisible_by_three(): 
+    a=[]
+    for x in range(1,40):
+    	if x % 3 == 0:
+    		a.append(x)
+    return a
+    print(divisible())
+
+def remove_duplicate():
+  x = ['a','b','a','e','d','b','c','e','f','g','h']
+new_list = []
+for item in x:
+    if item not in new_list:
+        new_list.append(item)
+        print(new_list())
+
+def list_comprehension();
+  a=dict()
+  b=(100,110,120,130,140,150)
+  for x in b:
+  a[x]=x%3
+  return x
+  print(x())
+
+
+
